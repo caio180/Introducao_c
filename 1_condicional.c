1ba5f250269b0aed4cefa45dfae6ededf341469e
caio180 <caio.lima.viana2009@gmail.com>
2026-07-19 21:22:19 -0300

    Utilizando o scanf

--- 1_condicional.c
+++ 1_condicional.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    int idade = 18;
+    int idade;
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
     if(idade >= 18){
         printf("Maior de idade");
     }
